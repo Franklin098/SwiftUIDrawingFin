fc0684329d4f3f1404f229c24c8942166a19bac3
Franklin Velasquez <iefranklin@hotmail.es>
2023-04-16 17:30:25 -0600

    Initial Commit

---
+++ Drawing Fin/Drawing_FinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drawing_FinApp.swift
+//  Drawing Fin
+//
+//  Created by Franklin Velásquez on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Drawing_FinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
